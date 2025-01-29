216252e14f5d70de67826183b72ab7e8adf2a010
khuraibet <129894070+khuraibet@users.noreply.github.com>
2025-01-29 23:53:00 +0000

    Added condition to check for negative numbers

--- FactorialLibrary/FactorialService.cs
+++ FactorialLibrary/FactorialService.cs
@@ -1,11 +1,13 @@
 ﻿namespace FactorialLibrary;
-
 public class FactorialService
 {
     public static int Factorial(int n) {
-        if(n ==1 || n==0) {
+        if(n<0) {
+            throw new InvalidOperationException("Negative numbers don't have proper factorial");
+        }
+        if(n==1 || n==0) {
             return 1;
         }
-        return n * Factorial(n - 1); // 5! - 5 * 4!
+        return n * Factorial(n-1);
     }
 }
